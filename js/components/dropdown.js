911e06a318c44215c47c67b55927c6249f13b91f
Michael <mt.dev.2023@gmail.com>
2025-10-16 18:14:17 +0200

    feat(dropdowns): dynamically populate dropdown lists from recipes data
    - Added functions to extract unique ingredients, appliances, and utensils from recipes.js
    - Implemented dropdown population
    - Ensured proper accessibility setup with aria attributes and unique IDs

--- js/components/dropdown.js
+++ js/components/dropdown.js
@@ -42,3 +42,71 @@ dropdownToggles.forEach((toggle, index) => {
 document.addEventListener('click', () => {
   closeAllDropdowns();
 });
+
+
+
+
+// == AFFICHAGE DES LISTES DANS LES DROPDOWNS
+// Import données
+import recipes from "../data/recipes.js";
+
+// --- Sélection des conteneurs du DOM ---
+const ingredientsList = document.querySelector(".ingredients-dropdown__list");
+const appliancesList = document.querySelector(".appliances-dropdown__list");
+const ustensilsList = document.querySelector(".ustensils-dropdown__list");
+
+// --- Extraction / dédoublonnage données ---
+function getUniqueIngredients(recipes) {
+  const ingredientsSet = new Set();
+  recipes.forEach(recipe => {
+    recipe.ingredients.forEach(item => ingredientsSet.add(item.ingredient));
+  });
+  return Array.from(ingredientsSet).sort();
+}
+
+function getUniqueAppliances(recipes) {
+  const appliancesSet = new Set();
+  recipes.forEach(recipe => appliancesSet.add(recipe.appliance));
+  return Array.from(appliancesSet).sort();
+}
+
+function getUniqueUstensils(recipes) {
+  const ustensilsSet = new Set();
+  recipes.forEach(recipe => {
+    recipe.ustensils.forEach(u => ustensilsSet.add(u));
+  });
+  return Array.from(ustensilsSet).sort();
+}
+
+// --- Injection ---
+function fillDropdown(listElement, items) {
+
+  listElement.innerHTML = "";
+
+  items.forEach(item => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+
+    button.className =
+      "w-full text-left p-4 font-family-sans text-sm text-text-black font-normal hover:bg-primary-yellow focus:bg-primary-yellow focus:outline-none";
+    button.setAttribute("role", "menuitem");
+    button.textContent = item;
+
+    li.appendChild(button);
+    listElement.appendChild(li);
+  });
+}
+
+// --- Fonction principale ---
+function initDropdowns() {
+  const ingredients = getUniqueIngredients(recipes);
+  const appliances = getUniqueAppliances(recipes);
+  const ustensils = getUniqueUstensils(recipes);
+
+  fillDropdown(ingredientsList, ingredients);
+  fillDropdown(appliancesList, appliances);
+  fillDropdown(ustensilsList, ustensils);
+}
+
+// --- Initialisation ---
+document.addEventListener("DOMContentLoaded", initDropdowns);
